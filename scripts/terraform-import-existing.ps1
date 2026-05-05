29007a0531f940fd444bd2cb5ee1878c10705364
Michael Yaacoub <csdmichael@gmail.com>
2026-05-05 13:53:27 -0700

    fix deploy

--- scripts/terraform-import-existing.ps1
+++ scripts/terraform-import-existing.ps1
@@ -13,8 +13,28 @@ $vnetName = "vnet-$namePrefix-$locationSlug"
 $logAnalyticsName = "log-$namePrefix-$locationSlug"
 $apiIdentityName = "msi-${namePrefix}api-$locationSlug"
 $uiIdentityName = "msi-${namePrefix}ui-$locationSlug"
-$foundryPrivateEndpointName = "pe-${namePrefix}fdry-$locationSlug"
-$searchPrivateEndpointName = "pe-${namePrefix}srch-$locationSlug"
+
+$searchServiceResourceId = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Search/searchServices/$($tfvars.search_service_name)"
+$privateEndpoints = @(az network private-endpoint list --resource-group $resourceGroup -o json | ConvertFrom-Json)
+
+function Get-PrivateEndpointIdByTargetResource {
+    param(
+        [string]$TargetResourceId
+    )
+
+    foreach ($privateEndpoint in $privateEndpoints) {
+        foreach ($connection in @($privateEndpoint.privateLinkServiceConnections)) {
+            if ($connection.privateLinkServiceId -eq $TargetResourceId) {
+                return $privateEndpoint.id
+            }
+        }
+    }
+
+    return $null
+}
+
+$foundryPrivateEndpointId = Get-PrivateEndpointIdByTargetResource -TargetResourceId $tfvars.foundry_account_resource_id
+$searchPrivateEndpointId = Get-PrivateEndpointIdByTargetResource -TargetResourceId $searchServiceResourceId
 
 $imports = @(
     @{ Address = 'azurerm_log_analytics_workspace.main'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.OperationalInsights/workspaces/$logAnalyticsName" },
@@ -27,8 +47,8 @@ $imports = @(
     @{ Address = 'azurerm_private_dns_zone.search'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/privateDnsZones/privatelink.search.windows.net" },
     @{ Address = 'azurerm_private_dns_zone_virtual_network_link.foundry'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/privateDnsZones/privatelink.services.ai.azure.com/virtualNetworkLinks/foundry-link" },
     @{ Address = 'azurerm_private_dns_zone_virtual_network_link.search'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/privateDnsZones/privatelink.search.windows.net/virtualNetworkLinks/search-link" },
-    @{ Address = 'azurerm_private_endpoint.foundry'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/privateEndpoints/$foundryPrivateEndpointName" },
-    @{ Address = 'azurerm_private_endpoint.search'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/privateEndpoints/$searchPrivateEndpointName" },
+    @{ Address = 'azurerm_private_endpoint.foundry'; Id = $foundryPrivateEndpointId },
+    @{ Address = 'azurerm_private_endpoint.search'; Id = $searchPrivateEndpointId },
     @{ Address = 'azurerm_linux_web_app.api'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Web/sites/$apiWebAppName" },
     @{ Address = 'azurerm_linux_web_app.ui'; Id = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Web/sites/$uiWebAppName" }
 )
@@ -41,6 +61,11 @@ foreach ($import in $imports) {
 
     if ($trackedAddresses -contains $address) {
         Write-Host "Already tracked: $address"
+        continue
+    }
+
+    if ([string]::IsNullOrWhiteSpace($resourceId)) {
+        Write-Host "Not present in Azure, skipping: $address"
         continue
     }
 
